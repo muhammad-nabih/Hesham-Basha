5ceda9913bd7c70bc695a6bc2629df9d580409f7
mohamed nabih <muhammadnabih2020@gmail.com>
2026-05-10 19:17:38 +0300

    refactor change icons for metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -52,19 +52,19 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       {
-        url: "/icon-light-32x32.png",
+        url: "/hesham-basha.png",
         media: "(prefers-color-scheme: light)",
       },
       {
-        url: "/icon-dark-32x32.png",
+        url: "/hesham-basha.png",
         media: "(prefers-color-scheme: dark)",
       },
       {
-        url: "/icon.svg",
+        url: "/hesham-basha.png",
         type: "image/svg+xml",
       },
     ],
-    apple: "/apple-icon.png",
+    apple: "/hesham-basha.png",
   },
 };
 
